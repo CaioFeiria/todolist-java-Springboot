85cdc9450bc2b2ca2b306ebbf9f7eca9c5da37e9
chF <caiofeiria@proton.me>
2024-08-16 14:05:29 -0300

    adicionando projeto ao github

---
+++ src/test/java/br/com/caiofeiria/todolist/TodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.caiofeiria.todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
